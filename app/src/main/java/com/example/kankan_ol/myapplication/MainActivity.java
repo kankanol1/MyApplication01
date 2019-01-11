3d48084cf6f4ed252ab6788ceef45469b5aa33f4
kankanol1 <1317894008@qq.com>
2019-01-11 22:35:30 +0800

    20190111_android

--- app/src/main/java/com/example/kankan_ol/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kankan_ol/myapplication/MainActivity.java
@@ -10,11 +10,13 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView t1 = (TextView)findViewById(R.id.text1);
+
+        TextView t1 = findViewById(R.id.text1);
         String s1 = "<font color='blue'><b>代码如诗你如画</b></font><br>";
         s1 += "<a href = 'https://kankan.fun'>kankan's 主页</a>";
         t1.setText(Html.fromHtml(s1));
