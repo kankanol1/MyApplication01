78b1da807cda57852aa32ca090e204bf138bc200
kankanol1 <1317894008@qq.com>
2019-01-12 10:07:13 +0800

    20190112_android

--- app/src/main/java/com/example/kankan_ol/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kankan_ol/myapplication/MainActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
     @Override
-    
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -20,7 +21,8 @@ public class MainActivity extends Activity {
         String s1 = "<font color='blue'><b>代码如诗你如画</b></font><br>";
         s1 += "<a href = 'https://kankan.fun'>kankan's 主页</a>";
         t1.setText(Html.fromHtml(s1));
-
         t1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Log.v("Fanny", String.valueOf(t1.getText()));
     }
 }
